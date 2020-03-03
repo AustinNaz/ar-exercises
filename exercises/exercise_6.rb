0aefd55d194cd6239e99f8231dde5ee897935016
Austin <Nazanator@hotmail.com>
2020-03-03 04:08:45 +0000

    Add relations between tables and seed data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Tots", last_name: "Totter", hourly_rate: 50)
+@store1.employees.create(first_name: "That", last_name: "Chick", hourly_rate: 62)
+
+@store2.employees.create(first_name: "Me", last_name: "You", hourly_rate: 30)
+@store2.employees.create(first_name: "Hello", last_name: "Person", hourly_rate: 40)
